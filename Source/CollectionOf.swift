f0e5f83fc03717d731fef16d769462a456c1ad9a
Neil Pankey <npankey@gmail.com>
2015-05-20 12:20:40 -0700

    MutableCollectionOf

--- Source/CollectionOf.swift
+++ Source/CollectionOf.swift
@@ -6,12 +6,12 @@
 //  Copyright (c) 2015 Neil Pankey. All rights reserved.
 //
 
-/// A type-erased collection type.
+/// A type-erased collection.
 public struct CollectionOf<T, Index: ForwardIndexType>: CollectionType {
     private let _generate: () -> GeneratorOf<T>
-    private let _subscript: (Index) -> T
     private let _startIndex: () -> Index
     private let _endIndex: () -> Index
+    private let _subscript: (Index) -> T
 
     public init<C: CollectionType where C.Generator.Element == T, C.Index == Index>(_ base: C) {
         _generate = { GeneratorOf(base.generate()) }
@@ -32,7 +32,45 @@ public struct CollectionOf<T, Index: ForwardIndexType>: CollectionType {
         return _endIndex()
     }
 
-    public subscript (index: Index) -> T {
+    public subscript(index: Index) -> T {
         return _subscript(index)
+    }
+}
+
+/// A type-erased mutable collection.
+public struct MutableCollectionOf<T, Index: ForwardIndexType>: MutableCollectionType {
+    private let _generate: () -> GeneratorOf<T>
+    private let _startIndex: () -> Index
+    private let _endIndex: () -> Index
+    private let _subscript: (Index) -> T
+    private let _mutate: (Index, T) -> ()
+
+    public init<C: MutableCollectionType where C.Generator.Element == T, C.Index == Index>(var _ base: C) {
+        _generate = { GeneratorOf(base.generate()) }
+        _startIndex = { base.startIndex }
+        _endIndex = { base.endIndex }
+        _subscript = { base[$0] }
+        _mutate = { (index, value) in base[index] = value }
+    }
+
+    public func generate() -> GeneratorOf<T> {
+        return _generate()
+    }
+
+    public var startIndex: Index {
+        return _startIndex()
+    }
+
+    public var endIndex: Index {
+        return _endIndex()
+    }
+
+    public subscript(index: Index) -> T {
+        get {
+            return _subscript(index)
+        }
+        set {
+            _mutate(index, newValue)
+        }
     }
 }
