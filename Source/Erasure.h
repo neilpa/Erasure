667222b44dc5e3a4b8bd4674c48912074742dc3d
Neil Pankey <npankey@gmail.com>
2015-05-20 11:51:50 -0700

    Xcode project

---
+++ Source/Erasure.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for Erasure.
+extern double ErasureVersionNumber;
+
+/// Project version string for Erasure.
+extern const unsigned char ErasureVersionString[];
+
